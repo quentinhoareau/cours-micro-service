fa4d665367bc930fe9fa742ba5fd29dc122052fc
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2024-03-11 10:42:48 +0100

    first commit

---
+++ src/main/java/org/example/coursmicroservice/CoursMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.coursmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/coursmicroservice/CoursMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coursmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
