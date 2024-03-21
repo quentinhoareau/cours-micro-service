f408243e0cb5c7e717d48a0b675f8d1d8097032b
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2024-03-21 11:01:52 +0100

    hot-reload

--- customer/src/main/java/org/example/customer/controllers/ConfigTestController.java
+++ customer/src/main/java/org/example/customer/controllers/ConfigTestController.java
@@ -1,12 +1,14 @@
 package org.example.customer.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigTestController {
     @Value("${a}")
     private int a;
