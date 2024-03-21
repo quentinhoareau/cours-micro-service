89349fee8c86073f1155c043f7091ea6cf5c5a8c
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2024-03-21 10:54:27 +0100

    config

---
+++ config/src/main/java/org/example/config/controllers/ConfigTestController.java
@@ -0,0 +1,22 @@
+package org.example.config.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+//@RestController
+//public class ConfigTestController {
+//    @Value("${a}")
+//    private int a;
+//    @Value("${b}")
+//    private int b;
+//    @Value("${c}")
+//    private int c;
+//
+//    //@GetMapping("/testConfig")
+//    //public Map<String, Integer> testConfig() {
+//      //  return Map.of("a", a, "b", b, "c", c);
+//    //}
+//}
